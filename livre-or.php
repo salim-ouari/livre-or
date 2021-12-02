51cef05f042a928b656db6cb4ef40fb96dd84b38
salim-ouari <salim.ouari@laplateforme.io>
2021-12-02 16:40:03 +0100

    tryhard

--- livre-or.php
+++ livre-or.php
@@ -3,3 +3,18 @@ récent au plus ancien. Chaque commentaire doit être composé d’un texte
 “posté le `jour/mois/année` par `utilisateur`” suivi du commentaire. Si
 l’utilisateur est connecté, sur cette page figure également un lien vers la
 page d’ajout de commentaire. -->
+
+
+
+<?php
+if (isset($_POST['envoyer'])) {
+
+    $id = $_SESSION['id_user'];
+    $msg = $_POST['message'];
+    $date = date('Y-m-d');
+    $heure = date('H:i');
+    $req1 = mysqli_query($cn, "insert into message values (NULL,'$msg','$date','$heure','$id')");
+    header("location:forum.php");
+}
+
+?>
