789089fca43cee01e8c7cf6e032d84a0a9a742b2
salim-ouari <salim.ouari@laplateforme.io>
2021-12-04 20:59:23 +0100

    php livre

--- livre-or.php
+++ livre-or.php
@@ -4,17 +4,56 @@ récent au plus ancien. Chaque commentaire doit être composé d’un texte
 l’utilisateur est connecté, sur cette page figure également un lien vers la
 page d’ajout de commentaire. -->
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Livre-or</title>
+</head>
 
-<?php
-if (isset($_POST['envoyer'])) {
+<body>
 
-    $id = $_SESSION['id_user'];
-    $msg = $_POST['message'];
-    $date = date('Y-m-d');
-    $heure = date('H:i');
-    $req1 = mysqli_query($cn, "insert into message values (NULL,'$msg','$date','$heure','$id')");
-    header("location:forum.php");
-}
+</body>
+<?php include 'header.php'; ?>
 
-?>
+<main>
+    <?php
+    // **************connection à la BDD*******************
+    require('connect.php');
+
+    $requete = mysqli_query($bdd, "SELECT * FROM utilisateurs INNER JOIN 'commentaires' WHERE 'utilisateurs.id' = 'commentaires.id_utlisateur' ORDER BY 
+'commentaires.date' DESC");
+
+    $comment = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+
+    foreach ($comment as $key => $value) {
+        echo    '<div id = "table">
+                    <table>
+                    <thead>
+                    <tr>
+                        <th>' . $value['login'] . '</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <td>' . $value['commentaire'] . ' ' . 'Poster le ' . ' ' . date_format(date_create($value['date']), 'd/m/Y H:i:s') . '</td>
+                    </tbody>
+                    </table>
+            </div>';
+    }
+    if (empty($_SESSION)) {
+        echo '  <div class="centre">
+                        <a href="connexion.php"><input id ="livre" type="submit" name= submit value="connecter vous ou inscrivez-vous pour poster un commentaires"></a>
+                    </div>';
+    } else {
+        echo   '<div class="centre">
+                        <a href="commentaire.php"><input id ="livre" type="submit" name= submit value="Poster un commentaires"></a>
+                    </div>';
+    }
+    ?>
+</main>
+
+</html>
