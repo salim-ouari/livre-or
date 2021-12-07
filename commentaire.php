af9a9b4ae7219635200c282c6ca609b3ed57f79b
salim-ouari <salim.ouari@laplateforme.io>
2021-12-07 09:39:01 +0100

    post com ok

--- commentaire.php
+++ commentaire.php
@@ -5,33 +5,41 @@ commentaires. -->
 
 <?php
 session_start();
+var_dump($_SESSION['user']['id']);
+
+/* Condition if qui permet de se deconnecter */
+if (isset($_POST['deconnexion'])) {
+
+    session_destroy();
+    header('location: index.php');
+}
+
 // connecte toi à la bdd
 require('connect.php');
 $error = '';
 
-if (isset($_POST['commentaire'])) {
-    $comment = $_POST['commentaire'];
-    date_default_timezone_set('Europe/Paris');
-    $date = date('Y-m-d H:i:s');
-    $id = $_SESSION['utilisateurs']['id'];
-    $requete = mysqli_query($bdd, "INSERT INTO commentaires(commentaire, id_utilisateur, date) VALUES ('$comment', '$id', '$date')");
-}
-
-if (isset($_POST['submit'])) { // Si on presse sur le bouton commenter
-
-    header('Location: livre-or.php');
-
-    $comment = htmlspecialchars($_POST['comment']);
-}
-if (empty($_SESSION)) {
-    header('Location: index.php');
-    exit();
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['comment'])) {
+        $comment = $_POST['comment'];
+        // date_default_timezone_set('Europe/Paris');
+        // $date = date('Y-m-d H:i:s');
+        $id = $_SESSION['user']['id'];
+        $requete = mysqli_query($bdd, "INSERT INTO commentaires(commentaire, id_utilisateur, date) VALUES ('$comment', '$id', NOW())");
+        header('Location: livre-or.php');
+        var_dump($requete);
+    } else {
+        $error = 'veuillez remplir le champs';
+    }
 }
 
 
-//  alors insére le com dans la base de donnée
+
 
 ?>
+
+<!-- // alors insére le com dans la base de donnée -->
+
+
 
 <!DOCTYPE html>
 <html>
@@ -46,21 +54,23 @@ if (empty($_SESSION)) {
 </head>
 
 <body>
-    <?php include 'header.php'; ?>
-
+    <header>
+        <?php include 'header.php'; ?>
+    </header>
     <div class="wrapper">
         <form action="" method="POST" class="form">
             <div class="row">
                 <div class="input-group textarea">
                     <label for="comment">Votre commentaire</label>
-                    <textarea id="comment" name="comment" placeholder="" required></textarea>
+                    <textarea id="comment" name="comment" placeholder="Tapez ici votre commentaire..." required></textarea>
                 </div>
                 <div class="input-group">
                     <button name="submit" class="btn">Poster commentaire</button>
                 </div>
         </form>
-
     </div>
+    <form class="deco" action="" method="post"><button type="submit" name="deconnexion">Déconnexion</button></form>
+
 </body>
 
 </html>
