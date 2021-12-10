bc03d49a7c98c82de48c7c0e3e5bbae438580d90
salim-ouari <salim.ouari@laplateforme.io>
2021-12-10 08:36:25 +0100

    modif icons

--- footer.php
+++ footer.php
@@ -3,20 +3,20 @@
 
     <div id="icons">
         <a href="http://www.twitter.fr" target="_blank">
-            <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" /></a>
+            <img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
 
         <a href="http://www.instagram.com" target="_blank">
-            <img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png" /></a>
+            <img src="https://img.icons8.com/clouds/100/000000/instagram-new--v3.png" /></a>
 
         <a href="http://www.facebook.fr" target="_blank">
-            <img src="https://img.icons8.com/fluency/48/000000/facebook.png" /></a>
+            <img src="https://img.icons8.com/clouds/110/000000/facebook.png" /></a>
 
         <a href="https://github.com/salim-ouari/voyages.git" target="_blank">
-            <img src="https://img.icons8.com/color-glass/48/000000/github.png" /></a>
+            <img src="https://img.icons8.com/bubbles/100/000000/github.png" /></a>
     </div>
 
     <div class="wrapper">
-        <h1 class="h1foot">My World</h1>
+        <h1 class="h1foot">MON LIVRE D'OR</h1>
         <div class="copyright">Copyright © 2021. Tous droits réservés.</div>
     </div>
 </footer>
