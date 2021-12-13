e35ae8b6fed7b6fd2ede437631e2cc7a40847a27
salim-ouari <salim.ouari@laplateforme.io>
2021-12-13 11:57:13 +0100

    plesk ok

--- connect.php
+++ connect.php
@@ -1,7 +1,8 @@
 <?php
 
-//***********************bdd plesk***********************
-// $bdd = mysqli_connect('localhost', 'salim-ouari', "Zidane07@", 'salim-ouari_moduleconnexion');
+// ***********************bdd plesk***********************
+$bdd = mysqli_connect('localhost', 'salim-ouari', "Zidane07@", 'salim-ouari_moduleconnexion');
+
 //   **********************bddlocal***********************
-$bdd = mysqli_connect('localhost', 'root', '', 'livreor');
-mysqli_set_charset($bdd, 'utf8');
+// $bdd = mysqli_connect('localhost', 'root', '', 'livreor');
+// mysqli_set_charset($bdd, 'utf8');
