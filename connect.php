9e10ba8151c9c5c993fb4f5a706f9ee9dfec2e4d
salim-ouari <salim.ouari@laplateforme.io>
2021-12-13 13:55:08 +0100

    projet plesk

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // ***********************bdd plesk***********************
-$bdd = mysqli_connect('localhost', 'salim-ouari', "Zidane07@", 'salim-ouari_moduleconnexion');
+$bdd = mysqli_connect('localhost', 'salim-ouari2', "Zidane07@", 'salim-ouari_livre-or');
 
 //   **********************bddlocal***********************
 // $bdd = mysqli_connect('localhost', 'root', '', 'livreor');
